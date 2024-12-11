7ae4f7443db642dcf0c2b25efcd8882000d1c10c
LuiZzz <luisenrriquejq@gmail.com>
2024-12-10 20:33:34 -0500

    Se creo una barra basica para la navegacion de la aplicacion

--- roleapp/App.js
+++ roleapp/App.js
@@ -12,33 +12,133 @@ import { Statistics } from "./screens/Statistics";
 
 export default function App() {
   const Tab = createBottomTabNavigator();
+
+  const CustomTabBarButton = ({ children, onPress }) => {
+    return (
+      <TouchableOpacity
+        style={{ top: -30, justifyContent: "center", alignItems: "center" }}
+        onPress={onPress}
+      >
+        <View
+          style={{
+            width: 80,
+            height: 80,
+            borderRadius: 40,
+            backgroundColor: "#e32f45",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {children}
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   function MyTabs() {
     return (
-      <Tab.Navigator>
-        <Tab.Screen name="Daylies" component={Daylies} />
-        {/* <Tab.Screen name="Create" component={CreateTask} /> */}
-        <Tab.Screen name="To Do's" component={ToDos} />
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            // padding: 20,
+            paddingTop: 25,
+            position: "absolute",
+            bottom: 15,
+            left: 15,
+            right: 15,
+            elevation: 0,
+            backgroundColor: "#faf",
+            borderRadius: 10,
+            height: "auto",
+          },
+        }}
+      >
+        <Tab.Screen
+          name="Daylies"
+          component={Daylies}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  alignItems: "center",
+                  // justifyContent: "center",
+                  // margin: 10,
+                }}
+              >
+                <Ionicons
+                  name="checkmark-circle-outline"
+                  size={32}
+                  color={focused ? "green" : "gray"}
+                />
+              </View>
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Create"
+          component={CreateTask}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return <Ionicons name="add-outline" size={60} color="white" />;
+            },
+            tabBarButton: (props) => <CustomTabBarButton {...props} />,
+          }}
+        ></Tab.Screen>
+
+        <Tab.Screen
+          name="To Do's"
+          component={ToDos}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  // margin: 10,
+                }}
+              >
+                <Ionicons
+                  name="checkmark-circle-outline"
+                  size={32}
+                  color={focused ? "green" : "gray"}
+                />
+                <Text
+                  style={{ fontSize: 20, color: focused ? "green" : "gray" }}
+                >
+                  Dayli
+                </Text>
+              </View>
+            ),
+          }}
+        />
       </Tab.Navigator>
     );
   }
 
   const Drawer = createDrawerNavigator();
 
-  function SettingsDrawer() {
-    return (
-      <Drawer.Navigator>
-        <Drawer.Screen name="Statistics" component={Statistics} />
-        {/* <Drawer.Screen name="Account" component={AccountScreen} /> */}
-      </Drawer.Navigator>
-    );
-  }
-
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Hsome" component={Daylies} />
-        <Tab.Screen name="Settings" component={ToDos} />
-      </Tab.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={MyTabs} />
+        <Drawer.Screen name="Statistics" component={Statistics} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: "#7F5DF0",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  },
+});
